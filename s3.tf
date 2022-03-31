495344fe5f3d65dffb84dc7b4687e647d06f6329
Your Name <you@example.com>
2022-03-31 18:55:54 +0530

    My S3 Bucket

---
+++ s3.tf
@@ -0,0 +1,14 @@
+resource aws_s3_bucket "b1"{
+      bucket = "hsbc-sheshi2809"
+      
+}
+resource aws_s3_bucket_policy "b1p1"{
+	bucket =aws_s3_bucket.b1.bucket
+	policy =file("./bpolicy.json")
+}
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.b1.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
